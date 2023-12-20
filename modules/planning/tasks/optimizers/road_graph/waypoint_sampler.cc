246ac513aac79bcd124a849b23171995604a91e6
Titicaca-Lee <46878324+Titicaca-Lee@users.noreply.github.com>
2023-12-20 14:50:34 +0800

    Update waypoint_sampler.cc

--- modules/planning/tasks/optimizers/road_graph/waypoint_sampler.cc
+++ modules/planning/tasks/optimizers/road_graph/waypoint_sampler.cc
@@ -52,48 +52,56 @@ bool WaypointSampler::SamplePathWaypoints(
   points->insert(points->begin(), std::vector<common::SLPoint>{init_sl_point_});
 
   const double kMinSampleDistance = 40.0;
+  //确定要规划的纵向距离，8秒*init_v或40米
   const double total_length = std::fmin(
       init_sl_point_.s() + std::fmax(init_point.v() * 8.0, kMinSampleDistance),
       reference_line_info_->reference_line().Length());
   const auto &vehicle_config =
       common::VehicleConfigHelper::Instance()->GetConfig();
+  //车宽的一半
   const double half_adc_width = vehicle_config.vehicle_param().width() / 2.0;
+  //每一纵列撒点数
   const double num_sample_per_level =
       FLAGS_use_navigation_mode ? config_.navigator_sample_num_each_level()
                                 : config_.sample_points_num_each_level();
 
   static constexpr double kSamplePointLookForwardTime = 4.0;
+  //纵向采样间隔，与速度有关
   const double level_distance =
       common::math::Clamp(init_point.v() * kSamplePointLookForwardTime,
                           config_.step_length_min(), config_.step_length_max());
-
+  //初始s
   double accumulated_s = init_sl_point_.s();
   double prev_s = accumulated_s;
 
   static constexpr size_t kNumLevel = 3;
   for (size_t i = 0; i < kNumLevel && accumulated_s < total_length; ++i) {
     accumulated_s += level_distance;
+    //s增加时的越界处理
     if (accumulated_s + level_distance / 2.0 > total_length) {
       accumulated_s = total_length;
     }
     const double s = std::fmin(accumulated_s, total_length);
-    static constexpr double kMinAllowedSampleStep = 1.0;
+    static constexpr double kMinAllowedSampleStep = 1.0;//最小采样间隔
     if (std::fabs(s - prev_s) < kMinAllowedSampleStep) {
-      continue;
+      continue;//采样间隔过小则不进行处理
     }
     prev_s = s;
 
     double left_width = 0.0;
     double right_width = 0.0;
+    //从参考轨迹得到道路左右界
     reference_line_info_->reference_line().GetLaneWidth(s, &left_width,
                                                         &right_width);
 
-    static constexpr double kBoundaryBuff = 0.20;
+    static constexpr double kBoundaryBuff = 0.20;//膨胀区域
+    //计算实际的可撒点的左右边界，减去车宽和膨胀区域
     const double eff_right_width = right_width - half_adc_width - kBoundaryBuff;
     const double eff_left_width = left_width - half_adc_width - kBoundaryBuff;
 
     // the heuristic shift of L for lane change scenarios
     const double delta_dl = 1.2 / 20.0;
+    //换道场景的L变化量，（初始斜率+预设变化率delta_dl）* level_distance
     const double kChangeLaneDeltaL = common::math::Clamp(
         level_distance * (std::fabs(init_frenet_frame_point_.dl()) + delta_dl),
         1.2, 3.5);
@@ -108,7 +116,7 @@ bool WaypointSampler::SamplePathWaypoints(
     double sample_left_boundary = eff_left_width;
 
     static constexpr double kLargeDeviationL = 1.75;
-    static constexpr double kTwentyMilesPerHour = 8.94;
+    static constexpr double kTwentyMilesPerHour = 8.94;//速度1miles = 1.6km(1英里 = 1.6千米)
     if (reference_line_info_->IsChangeLanePath() ||
         std::fabs(init_sl_point_.l()) > kLargeDeviationL) {
       if (injector_->ego_info()->start_point().v() > kTwentyMilesPerHour) {
@@ -129,20 +137,24 @@ bool WaypointSampler::SamplePathWaypoints(
     std::vector<double> sample_l;
     if (reference_line_info_->IsChangeLanePath() &&
         LaneChangeDecider::IsClearToChangeLane(reference_line_info_)) {
+      //换道情况下
       sample_l.push_back(reference_line_info_->OffsetToOtherReferenceLine());
-    } else {
+    } else {//不换道
+      //从左右边界进行均匀切分，返回sample_l
       common::util::uniform_slice(
           sample_right_boundary, sample_left_boundary,
           static_cast<uint32_t>(num_sample_per_level - 1), &sample_l);
     }
     std::vector<common::SLPoint> level_points;
     planning_internal::SampleLayerDebug sample_layer_debug;
+    //依次把每列的点存入level_points
     for (size_t j = 0; j < sample_l.size(); ++j) {
       common::SLPoint sl =
           common::util::PointFactory::ToSLPoint(s, sample_l[j]);
       sample_layer_debug.add_sl_point()->CopyFrom(sl);
       level_points.push_back(std::move(sl));
     }
+    //把level_points压入points，返回采样结果
     if (!level_points.empty()) {
       planning_debug_->mutable_planning_data()
           ->mutable_dp_poly_graph()
